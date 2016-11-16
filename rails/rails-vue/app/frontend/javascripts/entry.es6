3c869b3888d61afa7eb2559852b99a3b9f65f828
manderson <manderson@leapfrogonline.com>
2016-11-16 13:44:12 -0600

    es6 entry point set up via webpack

---
+++ rails/rails-vue/app/frontend/javascripts/entry.es6
@@ -0,0 +1,7 @@
+var _ = require('lodash');
+
+var count = 4;
+
+_.times(count, function(i) {
+  console.log(i);
+});
---
+++ rails/rails-vue/webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = module.exports = {
+  // the base path which will be used to resolve entry points
+  context: __dirname,
+  // the main entry point for frontend JS
+  entry: ['babel-polyfill', './app/frontend/javascripts/entry.es6'],
+};
+
+config.output = {
+  // this is our app/assets/javascripts directory, which is part of the Sprockets pipeline
+  path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+  // the filename of the compiled bundle, e.g. app/assets/javascripts/bundle.js
+  filename: 'bundle.js',
+  // if the webpack code-splitting feature is enabled, this is the path it'll use to download bundles
+  publicPath: '/assets',
+};
+
+config.resolve = {
+  // tell webpack which extensions to auto search when it resolves modules. With this,
+  // you'll be able to do `require('./utils')` instead of `require('./utils.js')`
+  extensions: ['', '.js', '.es6'],
+  // by default, webpack will search in `web_modules` and `node_modules`. Because we're using
+  // Bower, we want it to look in there too
+  modulesDirectories: [ 'node_modules' ],
+};
+
+config.module = {
+  loaders: [
+    { test: /\.es6$/,
+      loader: 'babel-loader',
+      include: path.resolve(__dirname, 'app/frontend'),
+      query: {
+        presets: ['es2015']
+      }
+    }
+  ]
+}
