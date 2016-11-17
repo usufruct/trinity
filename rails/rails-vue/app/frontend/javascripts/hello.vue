bf72416cc250562db8c97ea9cbeeb1c6d9064f29
manderson <manderson@leapfrogonline.com>
2016-11-17 15:39:00 -0600

    vue works with rails

---
+++ rails/rails-vue/app/frontend/javascripts/hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">hello {{ msg }}</div>
+</template>
+
+<script>
+export default {
+  props: ['msg'],
+  data () {
+    return {};
+  }
+}
+</script>
+
+<style>
+.hello {
+  color: blue;
+}
+</style>
